8543696771497beea312e802ccb5a16f54225670
nurislombek006-pixel <nurislombek006@gmail.com>
2026-06-19 18:02:01 +0500

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-// env.js
-// Здесь нет токена бота и нет секретов.
-// BACKEND_URL должен вести на Cloudflare Worker из папки cloudflare-backend-private.
-
-(function () {
-  window.__APP_ENV = {
-    BACKEND_URL: "https://quizbot-backend.application-site.workers.dev"
-  };
-})();
